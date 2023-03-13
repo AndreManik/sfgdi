1ec27ddd28228d0cff532579dce237e56be5e13d
manik4 <andrei.manik@orionic.com>
2023-03-13 15:27:46 +0300

    Refactor to Java configuration Primary Beans and Profiles

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,36 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingServiceImpl;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.services.I18nSpanishGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingServiceImpl;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile("ES")
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingServiceImpl constructorGreetingService () {
